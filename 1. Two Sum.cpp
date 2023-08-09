f32507b5eaeaa460cdf95faf51451cc720cb6c2a
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2023-08-09 23:14:57 +0530

    Update and rename 1. Two Sum to 1. Two Sum.cpp

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //BRUTE FORCE APPROACH
+        // int a=0;
+        // int b=0;
+        // for(int i=0;i<nums.size()-1;i++)
+        // {
+        //     for(int j=i+1;j<nums.size();j++)
+        //     {
+        //         if(nums[i]+nums[j]==target)
+        //         {
+        //             a=i;
+        //             b=j;
+        //         }
+        //     }
+        // }
+        // return {a,b};
+
+
+        //TWO POINTER APPROACH
+        vector<pair<int,int>> temp;
+        for(int i=0;i<nums.size();i++){
+            temp.push_back(make_pair(nums[i],i));
+        }
+        sort(temp.begin(),temp.end());
+        auto left=temp.begin(),right=temp.end();
+        right--;
+        while(left < right){
+            if(left->first + right->first == target){
+                return {left->second,right->second};
+            }
+            else if(left->first + right->first > target)
+            right--;
+            else
+            left++;
+        }
+        return {0};
+    }
+};
