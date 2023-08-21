a9078085fc37561c86960ed6ee7023124e7ee4ea
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2023-08-21 21:47:16 +0530

    Create 540. Single Element in a Sorted Array.cpp

---
+++ 540. Single Element in a Sorted Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        if(nums.size() == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[nums.size()-1] != nums[nums.size()-2]) return nums[nums.size()-1];
+        int low = 1,high = nums.size()-2;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid +1])
+            return nums[mid];
+            if((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid -1] == nums[mid]))
+            low = mid + 1;
+            else
+            high = mid - 1;
+        }
+        return -1;
+    }
+};
