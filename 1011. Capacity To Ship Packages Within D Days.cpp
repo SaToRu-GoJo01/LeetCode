1254b460a946f1ba09d092304113dd8d723ca8b6
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2023-08-26 16:23:28 +0530

    Create 1011. Capacity To Ship Packages Within D Days.cpp

---
+++ 1011. Capacity To Ship Packages Within D Days.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool possible(vector<int> weights,int d,int current){
+        int noOfDays = 1,load = 0;
+        for(auto i:weights){
+            if((load + i)<= current){
+                load += i;
+            }
+            else{
+                noOfDays++;
+                load=i;
+            }
+        }
+        return noOfDays <= d;
+    }
+    int shipWithinDays(vector<int>& weights, int d) {
+    int low = INT_MIN, high = 0,ans;
+    for(auto i: weights){
+        high += i;
+        low = max(i,low);
+    }
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if(possible(weights,d,mid)){
+            ans = mid;
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    return ans;
+    }
+};
