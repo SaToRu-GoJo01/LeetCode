53f4558b57483495e0d7d8fa94fe26c1ffff5618
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2023-10-15 12:04:27 +0530

    Create 451. Sort Characters By Frequency.cpp

---
+++ 451. Sort Characters By Frequency.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> mp;
+        for(auto i:s){
+            mp[i]++;
+        }
+        vector<pair<char,int>> sortedVector(mp.begin(),mp.end());
+        sort(sortedVector.begin(),sortedVector.end(),[](const pair<char,int> &a,const pair<char,int>&b){
+            return a.second > b.second;
+        });
+        s = "";
+        for(int i=0;i<sortedVector.size();i++){
+            while(sortedVector[i].second != 0){
+                s += sortedVector[i].first;
+                sortedVector[i].second--;
+            }
+        }
+        return s;
+    }
+};
