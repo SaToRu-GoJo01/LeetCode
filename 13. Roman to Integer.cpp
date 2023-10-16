2a822fc6f4fc7540e1494dcdaa0b63d6aa6d1ed5
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2023-10-16 09:04:47 +0530

    Create 13. Roman to Integer.cpp

---
+++ 13. Roman to Integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int sum=0;
+        unordered_map<char,int> romanNumber{
+            {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}
+        };
+        for(int i=0;i<s.length();i++)
+        {
+            if(romanNumber[s[i]] < romanNumber[s[i+1]])
+            sum = sum - romanNumber[s[i]];
+            else
+            sum = sum + romanNumber[s[i]];
+        }
+        return sum;
+    }
+};
