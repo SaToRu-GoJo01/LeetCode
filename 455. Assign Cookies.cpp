abd6717a2dfead6b46a02c42f1254599b0b24577
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2024-01-01 11:58:49 +0530

    Create 455. Assign Cookies.cpp

---
+++ 455. Assign Cookies.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int ans=0;
+        int i=0,j=0;
+        while(i < s.size() && j < g.size()){
+            if(s[i] >= g[j]){
+                ans++;
+                j++;
+            }
+            i++;
+        };
+        return ans;
+    }
+};
