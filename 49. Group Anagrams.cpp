fb0a1dcc983f630d7fe1a47011f58e5706292224
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2024-02-07 00:07:04 +0530

    Update 49. Group Anagrams.cpp

--- 49. Group Anagrams.cpp
+++ 49. Group Anagrams.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
+    string generate(string given){
+        vector<int>freq(26,0);
+        for(auto &i:given){
+            freq[i-'a']++;
+        }
+        string newString = "";
+        for(int i=0;i<26;i++){
+            if(freq[i] > 0)
+                newString += string(freq[i],i+'a');
+        }
+        return newString;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>> mp;
-        for(auto i : strs){
-            string word = i;
-            sort(word.begin(),word.end());
-            mp[word].push_back(i);
+        unordered_map<string,vector<string>> record;
+        for(auto &i:strs){
+            // USING SORTING
+            // string curr = i;
+            // sort(i.begin(),i.end());
+            // record[i].push_back(curr);
+
+            // USING FREQ VECTOR
+            string newString = generate(i);
+            record[newString].push_back(i);
         }
         vector<vector<string>> result;
-        for(auto i : mp){
+        for(auto &i:record){
             result.push_back(i.second);
         }
         return result;
