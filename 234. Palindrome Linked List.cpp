6c483563764f6080cf7b66254958cbfd652eb37c
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2024-02-11 15:48:00 +0530

    Update 234. Palindrome Linked List.cpp

--- 234. Palindrome Linked List.cpp
+++ 234. Palindrome Linked List.cpp
@@ -8,6 +8,8 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// APPROACH USING STACK
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
@@ -23,6 +25,39 @@ public:
             }
             st.pop();
             head = head->next;
+        }
+        return true;
+    }
+};
+
+// APPROACH USING REVERSE AND TWO POINTER
+class Solution {
+public:
+    ListNode* reverse(ListNode* head){
+        ListNode*curr = head;
+        ListNode*prev = NULL;
+        ListNode*nxt = NULL;
+        while(curr != NULL){
+            nxt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL || head->next == NULL) return true;
+        ListNode*slow = head;
+        ListNode*fast = head;
+        while(fast->next != NULL && fast->next->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode*head2 = reverse(slow->next);
+        while(head2 != NULL){
+            if(head->val != head2->val) return false;
+            head = head->next;
+            head2 = head2->next;
         }
         return true;
     }
