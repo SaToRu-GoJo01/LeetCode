09e2fc09c4189f2536de8759e95c48bb9809cb0a
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2024-02-13 05:10:26 +0530

    Update 169. Majority Element.cpp

--- 169. Majority Element.cpp
+++ 169. Majority Element.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(int n : nums){
-            mp[n]++;
+        // unordered_map<int,int> mp;
+        // for(int n : nums){
+        //     mp[n]++;
+        // }
+        // for(auto n : mp){
+        //     if(n.second > nums.size()/2)
+        //     return n.first;
+        // }
+        // return 0;
+
+        // Moore's Voting Algo.
+        int vote = 0,candidate = -1;
+        for(auto &i:nums){
+            if(vote == 0){
+                candidate = i;
+                vote++;
+            }
+            else if(candidate == i){
+                vote++;
+            }
+            else{
+                vote--;
+            }
         }
-        for(auto n : mp){
-            if(n.second > nums.size()/2)
-            return n.first;
+        int count = 0;
+        for(auto &i:nums){
+            if(i == candidate) count++;
         }
-        return 0;
+        if(count > nums.size()/2) return candidate;
+        return -1;
     }
 };
