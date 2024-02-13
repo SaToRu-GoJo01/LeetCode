fd1093c5edf342c060f27f8ab694a7732537a2f6
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2024-02-13 09:55:51 +0530

    Update 2108. Find First Palindromic String in the Array.cpp

--- 2108. Find First Palindromic String in the Array.cpp
+++ 2108. Find First Palindromic String in the Array.cpp
@@ -23,3 +23,17 @@ public:
         return "";
     }
 };
+
+// USING REVERSE FUNCTION
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+        for(auto &i:words){
+            string s = i;
+            reverse(i.begin(),i.end());
+            if(i == s) return s;
+        }
+
+        return "";
+    }
+};
