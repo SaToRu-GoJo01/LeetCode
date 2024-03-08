dc5e903fe3a0c37ba98b5b83936b43ab269fd2cf
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2024-03-08 14:32:00 +0530

    Create 240. Search a 2D Matrix II.cpp

---
+++ 240. Search a 2D Matrix II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int col = matrix[0].length-1;
+        int row = 0;
+        while(row < matrix.length && col >= 0){
+            if(matrix[row][col] == target) return true;
+            
+            else if(matrix[row][col] > target) col--;
+
+            else row++;
+        }
+        return false;
+    }
+}
