1e1817940150172d2884f6134e16cf162d04c351
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2024-04-21 00:20:38 +0530

    Create 1992. Find All Groups of Farmland.py

---
+++ 1992. Find All Groups of Farmland.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
+        def traversal(i,j):
+            nonlocal x, y
+            if (i >= len(land) or j >= len(land[0]) or i < 0 or j < 0 or visited[i][j] or land[i][j] == 0):
+                return
+            visited[i][j] = True
+            if(x <= i):
+                x = i
+            if(y <= j):
+                y = j
+            traversal(i-1,j)
+            traversal(i+1,j)
+            traversal(i,j+1)
+            traversal(i,j-1)
+        x,y=0,0
+        visited = [[False for _ in range(len(land[0]))] for _ in range(len(land))]
+        result = []
+        for i in range(len(land)):
+            for j in range(len(land[0])):
+                if(land[i][j] == 1 and not visited[i][j]):
+                    traversal(i,j)
+                    result.append([i,j,x,y])
+                    x,y=0,0
+        return result
