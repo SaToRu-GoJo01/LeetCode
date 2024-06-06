5a7aa0311f3bc7734ec8f05f88f876224f77128d
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2024-06-06 14:05:07 +0530

    Create 846. Hand of Straights.py

---
+++ 846. Hand of Straights.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+
+        hand.sort()
+
+        count = Counter(hand)
+
+        print(count)
+
+        for card in hand:
+            if count[card] == 0:
+                continue
+
+            for i in range(groupSize):
+                if count[card+i] > 0:
+                    count[card+i] -= 1
+                else:
+                    return False
+        return True
