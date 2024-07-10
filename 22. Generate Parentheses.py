ede140d6a88e962b9f445ecbff864119cfb3b2db
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2024-07-10 22:29:04 +0530

    Update 22. Generate Parentheses.py

--- 22. Generate Parentheses.py
+++ 22. Generate Parentheses.py
@@ -1,15 +1,17 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         result = []
-        def makeParenthesis(string,count):
+        def makeParenthesis(string,count,open,close):
+            if open < close:
+                return
             if count < 0:
                 return
             if len(string) == 2*n:
                 if count == 0:
                     result.append(string)
                 return
-            makeParenthesis(string+"(",count+1)
-            makeParenthesis(string+")",count-1)
+            makeParenthesis(string+"(",count+1,open+1,close)
+            makeParenthesis(string+")",count-1,open,close+1)
             return
-        makeParenthesis("",0)
+        makeParenthesis("",0,0,0)
         return result
