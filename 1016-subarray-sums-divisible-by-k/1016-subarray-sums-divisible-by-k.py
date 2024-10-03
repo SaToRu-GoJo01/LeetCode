56a37e18dda6ff92623f63f45de621e46c4f6754
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2024-10-04 00:16:33 +0530

    Time: 223 ms (84.74%), Space: 21.5 MB (69.01%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.py
@@ -1,31 +1,13 @@
-# class Solution:
-#     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-#         mp = {0:1}
-#         curr = 0
-#         count = 0
-#         for num in nums:
-#             curr += num
-#             mod = curr % k
-#             if mod in mp:
-#                 count += mp[mod]
-#                 mp[mod] += 1
-#             else:
-#                 mp[mod] = 1
-#         return count
+from collections import defaultdict
 class Solution:
-    def sub(nums: List[int], k: int) -> int:
-        ream=ans=0
-        d=defaultdict(int)
-        d[0]=1
-    
-        for i in nums:
-            ream=(ream+i)%k
-            ans+=d[ream]
-            d[ream]+=1
-    
-        return ans
-
-    sys.stdout=open('user.out','w')
-    for nums in map(loads,stdin):
-        print(sub(nums,int(stdin.readline())))
-    exit()
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        mp = defaultdict(int)
+        mp[0] = 1
+        curr = 0
+        count = 0
+        for num in nums:
+            curr = (curr+num)%k
+            count += mp[curr]
+            mp[curr] += 1
+            
+        return count
