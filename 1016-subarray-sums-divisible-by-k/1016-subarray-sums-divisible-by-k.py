c8e478d50f5ef4ceb918a86a3a21dddc2d2a41e9
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2024-10-04 00:29:00 +0530

    Time: 80 ms (99.84%), Space: 19.2 MB (99.88%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.py
@@ -1,13 +1,17 @@
-from collections import defaultdict
 class Solution:
-    def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        mp = defaultdict(int)
-        mp[0] = 1
-        curr = 0
-        count = 0
-        for num in nums:
-            curr = (curr+num)%k
-            count += mp[curr]
-            mp[curr] += 1
-            
-        return count
+    def sub(nums: List[int], k: int) -> int:
+        ream=ans=0
+        d=defaultdict(int)
+        d[0]=1
+    
+        for i in nums:
+            ream=(ream+i)%k
+            ans+=d[ream]
+            d[ream]+=1
+    
+        return ans
+
+    sys.stdout=open('user.out','w')
+    for nums in map(loads,stdin):
+        print(sub(nums,int(stdin.readline())))
+    exit()
