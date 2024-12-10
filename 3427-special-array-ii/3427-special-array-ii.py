e12970ff48ab619827996463e73aa724832551f7
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2024-12-11 00:20:35 +0530

    Time: 33 ms (88.55%), Space: 46.6 MB (55.04%) - LeetHub

---
+++ 3427-special-array-ii/3427-special-array-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        prefix_sum = []
+        oddEven = None
+        running = 0
+
+        for num in nums:
+            if num % 2 == oddEven:
+                running+=1
+            oddEven = num % 2    
+            prefix_sum.append(running)
+
+        print(prefix_sum)
+        ans = []
+
+        for x,y in queries:
+            if prefix_sum[x] == prefix_sum[y]:
+                ans.append(True)
+            else:
+                ans.append(False)
+        return ans
