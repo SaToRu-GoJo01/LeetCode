765d89ad3242602546d64d67841c278ec8185580
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2024-12-26 16:27:08 +0530

    Time: 100 ms (47.39%), Space: 45.9 MB (5.48%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,14 +1,22 @@
 class Solution {
-    public int recursion(int[] nums, int target, int curr, int sum){
+    public int recursion(int[] nums, int target, int curr, int sum ,Map<String,Integer> memo){
         if(curr >= nums.length){
             if(target == sum){
                 return 1;
             }
             return 0;
         }
-        return recursion(nums,target,curr+1,sum+nums[curr]) + recursion(nums,target,curr+1,sum-nums[curr]);
+        String key = sum + "," + curr;
+        if(memo.containsKey(key)){
+            return memo.get(key);
+        }
+        int add = recursion(nums,target,curr+1,sum+nums[curr],memo);
+        int sub = recursion(nums,target,curr+1,sum-nums[curr],memo);
+        memo.put(key,add + sub);
+        return add + sub;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return recursion(nums,target,0,0);
+        Map<String,Integer> memo = new HashMap<>();
+        return recursion(nums,target,0,0,memo);
     }
 }
