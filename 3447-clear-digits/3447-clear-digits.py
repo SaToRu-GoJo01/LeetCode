54804ddd096a0e4c06fbea77d2437fe5c187cd47
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2025-02-10 13:29:20 +0530

    Time: 0 ms (100%), Space: 17.9 MB (27.31%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.py
+++ 3447-clear-digits/3447-clear-digits.py
@@ -2,7 +2,7 @@ class Solution:
     def clearDigits(self, s: str) -> str:
         stack = []
         for i in s:
-            if i not in ['0','1','2','3','4','5','6','7','8','9']:
+            if i.isalpha():
                 stack.append(i)
             else:
                 stack.pop()
