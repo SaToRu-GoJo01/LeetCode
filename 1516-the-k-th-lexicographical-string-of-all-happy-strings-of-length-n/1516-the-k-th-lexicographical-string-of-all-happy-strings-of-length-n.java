dcd9e4c8d57f7e8f6128ed9c4cccaccd68de3147
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2025-02-19 21:57:37 +0530

    Time: 22 ms (29.84%), Space: 45.8 MB (9.11%) - LeetHub

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -0,0 +1,25 @@
+class Solution {
+    private int count = 0;
+    private String ans = null;
+    public void recursion(String abc,String curr,char added,int n,int k){
+        if(curr.length() == n){
+            ++count;
+            if(count == k){
+                ans = curr;
+            }
+            return;
+        }
+        for(int i = 0;i<3;i++){
+            if(added != abc.charAt(i)){
+                recursion(abc,curr+abc.charAt(i),abc.charAt(i),n,k);
+            }
+        }
+    }
+    public String getHappyString(int n, int k) {
+        recursion("abc","",'_',n,k);
+        if(ans != null){
+            return ans;
+        }
+        return "";
+    }
+}
