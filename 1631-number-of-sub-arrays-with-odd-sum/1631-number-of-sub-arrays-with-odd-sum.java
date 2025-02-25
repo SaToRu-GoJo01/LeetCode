a9b5e8df1f4c74e24fb52904d0672b25875b297d
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2025-02-25 21:16:22 +0530

    Time: 7 ms (83.08%), Space: 58.3 MB (35.36%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.java
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.java
@@ -3,16 +3,13 @@ class Solution {
         int MOD = 1000000007;
         int ans = 0;
         int n = arr.length;
-        int[] prefix_sum = new int[n];
-        int sum = 0;
-        for(int i =0;i<n;i++){
-            sum += arr[i];
-            prefix_sum[i] += sum; 
-        }
         int even = 1;
         int odd = 0;
+        int prefix_sum = 0;
+
         for(int i = 0;i<n;i++){
-            if(prefix_sum[i] % 2 == 0){
+            prefix_sum += arr[i];
+            if(prefix_sum % 2 == 0){
                 ans = (ans + odd) % MOD;
                 even++;
             }
@@ -21,6 +18,7 @@ class Solution {
                 odd += 1;
             }
         }
+
         return ans;
     }
 }
