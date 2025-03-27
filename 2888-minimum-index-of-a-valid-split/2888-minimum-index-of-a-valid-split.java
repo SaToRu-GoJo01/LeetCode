79672e3a147f9590e9fe156967957f03fedfdd05
Arnav Chaudhary <113378266+SaToRu-GoJo01@users.noreply.github.com>
2025-03-27 17:50:41 +0530

    Time: 27 ms (63.46%), Space: 62.2 MB (22.44%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -1,31 +1,37 @@
+import java.util.*;
+
 class Solution {
     public int minimumIndex(List<Integer> nums) {
-        Map<Integer,Integer> counter = new HashMap<>();
+        Map<Integer, Integer> counter = new HashMap<>();
         int n = nums.size();
         int count = 0;
         int curr = -1;
-        for(int i = 0;i<n;i++){
-            counter.put(nums.get(i),counter.getOrDefault(nums.get(i),0)+1);
-            if(count == 0){
+
+        for (int num : nums) {
+            counter.put(num, counter.getOrDefault(num, 0) + 1);
+            if (count == 0) {
+                curr = num;
+                count = 1;
+            } else if (curr == num) {
                 count++;
-                curr = nums.get(i);
-            }
-            else if(curr == nums.get(i)){
-                count++;
-            }
-            else{
+            } else {
                 count--;
             }
         }
-        
+
         int currCount = counter.get(curr);
+        if (currCount * 2 <= n) {
+            return -1;
+        }
+
         int tillNow = 0;
-        for(int i = 0;i<n;i++){
-            if(curr == nums.get(i)){
+        for (int i = 0; i < n; i++) {
+            if (nums.get(i) == curr) {
                 tillNow++;
-            }
-            if(2 * tillNow > (i+1) && 2 * (currCount - tillNow) > n - (i + 1)){
-                return i;
+                int leftSize = i + 1, rightSize = n - (i + 1);
+                if (2 * tillNow > leftSize && 2 * (currCount - tillNow) > rightSize) {
+                    return i;
+                }
             }
         }
         return -1;
